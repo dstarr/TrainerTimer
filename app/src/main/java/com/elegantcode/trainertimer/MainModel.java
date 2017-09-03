1128c09601de62ca1032442af654acaaa7f3dc74
David Starr <dxstarr@godaddy.com>
2017-09-03 14:57:47 -0700

    umbers working from model

--- app/src/main/java/com/elegantcode/trainertimer/MainModel.java
+++ app/src/main/java/com/elegantcode/trainertimer/MainModel.java
@@ -6,14 +6,41 @@ package com.elegantcode.trainertimer;
 
 public class MainModel {
 
+    private int minutes = 0;
+    private int seconds = 0;
 
+
+    public void addMinutes(int min) {
+        minutes += min;
+    }
+
+    public void addSeconds(int sec) {
+        seconds += sec;
+    }
 
     public String getMinutes() {
-        return "XX";
+
+        if(minutes < 0) { minutes = 0; }
+
+        return formatNumber(minutes);
     }
 
     public String getSeconds() {
-        return "XX";
+
+        if(seconds < 0) { seconds = 0; }
+
+        return formatNumber(seconds);
+    }
+
+    private String formatNumber(int number) {
+
+        String sNumber = String.valueOf(number);
+
+        if(number < 10) {
+            sNumber = "0" + sNumber;
+        }
+
+        return sNumber;
     }
 
 }
--- app/src/main/java/com/elegantcode/trainertimer/MainPresenter.java
+++ app/src/main/java/com/elegantcode/trainertimer/MainPresenter.java
@@ -14,49 +14,51 @@ public class MainPresenter {
     TextView tvMinutes;
     TextView tvSeconds;
 
+    MainModel model = new MainModel();
+
     public MainPresenter (TextView minutes, TextView seconds) {
 
         tvMinutes = minutes;
         tvSeconds = seconds;
+
+        tvMinutes.setText(model.getMinutes());
+        tvSeconds.setText(model.getSeconds());
     }
 
     public void handleNumberClick(int btnClicked) {
 
-        int currentSeconds = Integer.parseInt(tvSeconds.getText().toString());
-        int currentMinutes = Integer.parseInt(tvMinutes.getText().toString());
+//        int currentSeconds = Integer.parseInt(tvSeconds.getText().toString());
+//        int currentMinutes = Integer.parseInt(tvMinutes.getText().toString());
 
         switch(btnClicked) {
 
             case R.id.button_add10s:
-                currentSeconds += 10;
-                tvSeconds.setText(String.valueOf(currentSeconds));
+                model.addSeconds(10);
                 break;
 
             case R.id.button_minus10s:
-                currentSeconds -= 10;
-                tvSeconds.setText(String.valueOf(currentSeconds));
+                model.addSeconds(-10);
                 break;
 
             case R.id.button_add1m:
-                currentMinutes += 1;
-                tvMinutes.setText(String.valueOf(currentMinutes));
+                model.addMinutes(1);
                 break;
 
             case R.id.button_minus1m:
-                currentMinutes -= 1;
-                tvMinutes.setText(String.valueOf(currentMinutes));
+                model.addMinutes(-1);
                 break;
 
             case R.id.button_add5m:
-                currentMinutes += 5;
-                tvMinutes.setText(String.valueOf(currentMinutes));
+                model.addMinutes(5);
                 break;
 
             case R.id.button_minus5m:
-                currentMinutes -= 5;
-                tvMinutes.setText(String.valueOf(currentMinutes));
+                model.addMinutes(-5);
                 break;
 
         }
+
+        tvMinutes.setText(model.getMinutes());
+        tvSeconds.setText(model.getSeconds());
     }
 }
